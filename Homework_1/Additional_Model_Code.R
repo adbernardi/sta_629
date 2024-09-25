29d9baaee89bf6081e401349907e1499a6b2426e
adbernardi <anthony.d.bernardi@gmail.com>
2024-09-25 12:58:26 -0400

    Updating R code

--- Homework_1/Additional_Model_Code.R
+++ Homework_1/Additional_Model_Code.R
@@ -16,23 +16,12 @@ head(X)
 dim(X)
 dim(y)
 
-data("Prostate")
-class(Prostate$X)
-class(Prostate$y)
-
-# comparing 
+# doing relevant type conversions  
 class(X) # need to convert to matrix array 
 
 X <- as.matrix(X)
 class(y) # need to convert to numeric
 y <- y$V1
-
-
-# now we can do the actual modeling with the data loaded and get to plotting
-# and eventually scoring 
-# using another package to avoid the grouped covariates problem 
-# need to coerce y into a numeric 
-# trying another way 
 
 # trying the new way 
 mcp <- ncvreg(X, y) # MCP is default
@@ -40,7 +29,6 @@ coef(mcp, lambda = 0.05)
 summary(mcp , lambda = 0.1)
 
 # now trying plotting 
-# plot(mcp)
 # Extract coefficients
 coefs <- coef(mcp)
 p <- length(colnames(X)) # number of predictors
@@ -50,12 +38,6 @@ plot(NA, xlim=c(1, length(mcp$lambda)), ylim=range(coefs), xlab="Lambda Index", 
      , main = "MCP Regularization Path")
 matlines(1:length(mcp$lambda), t(coefs[-1, ]), type="l", col=1:p, lty=1 , xlab = "Lambda Index", 
          ylab = "Coefficients")
-
-
-# this works! 
-
-mc_model2 <- ncvreg(X, y) # MCP default 
-# maybe we're having the transpose problem again
 
 # now we can do this same thing with SCAD 
 scad <- ncvreg(X, y, penalty = "SCAD") # using the SCAD model now
@@ -67,26 +49,10 @@ coefs_s <- coef(scad)
 plot(NA, xlim = c(1, length(scad$lambda)), ylim=range(coefs_s), xlab="Lambda Index", ylab="Coefficients"
      , main = "SCAD Regularization Path")
 matlines(1:length(scad$lambda), t(coefs[-1, ]), type="l", col=1:p, lty=1)
-plot(mc_model2, type = "scale", vertical.line = TRUE)
-summary(mc_model2)
 
 # it appears we have many 0 coefficients and a sparse model, which, when 
 # selecting specific alpha, allows us to see which variables are most 
 # strongly correlated with the outcome variable
 
-# we can now try this with the SCAD penalty to observe any differences
-mc_model_scad <- ncvreg(X, y, penalty = "SCAD")
-
-# can now observe any differences 
-plot(mc_model_scad, type = "scale")
-
-summary(mc_model_scad)
-
-# we can try cross-validating to get around the lambda issue 
-cvfit <- cv.ncvreg(X, y)
-summary(cvfit)
-
-cvfit_scad <- cv.ncvreg(X, y, penalty = "SCAD")
-summary(cvfit_scad)
 
 
