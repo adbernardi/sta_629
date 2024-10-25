b140899d23d508d6377735d1e5f3eb50d4836bd0
adbernardi <anthony.d.bernardi@gmail.com>
2024-10-25 16:11:45 -0400

    More HW3

--- Homework_3/HW3_Code.py
+++ Homework_3/HW3_Code.py
@@ -69,7 +69,7 @@ print(y_test.shape)
 # noow we can move on to the cross-validation and tuning
 # we will start with the K Nearest Neighbors model, and we will use cross-validation to find the best value of K 
 # we can now start with the grid search for the optimal KNN parameters given the data 
-from sklearn.model_selection import GridSearchCV 
+from sklearn.model_selection import GridSearchCV, train_test_split 
 from sklearn.neighbors import KNeighborsClassifier 
 
 # establishing the parameter grid 
@@ -146,31 +146,183 @@ print(lr_grid_en.best_score_)
 # l2 regularization performed best in a cross-validation comparison, but we will likely need to compare with an elastic net model. 
 
 # we will now try cross-validation with a linear SVM model 
-from sklearn.svm import SVC 
+from sklearn.svm import LinearSVC  
 
 # establishing our parameter grid as before 
-svc_param_grid = {'C': [0.1, 1, 10, 100, 1000],
-                  'gamma': [1, 0.1, 0.01, 0.001, 0.0001]}
+'''
+lin_svc_param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 
+                      'penalty': ['l1', 'l2'], 
+                      'loss': ['hinge', 'squared_hinge']}
+
+lin_svc_gs = GridSearchCV(LinearSVC(), lin_svc_param_grid, 
+                          cv = 5)
+
+lin_svc_grid = lin_svc_gs.fit(X_train, y_train) 
+
+print("Finding the best Linear SVM model...") 
+print(lin_svc_grid.best_params_) 
+print(lin_svc_grid.best_score_) 
+'''
+# we can now see that the best linear SVC has a C value of 10 and a gamma value of 0.01.
+# we will make predictions with this best model now 
+#lin_svc_best = LinearSVC(C = 0.01, loss = 'squared_hinge', penalty = 'l2').fit(X_train, y_train) 
+#y_pred_lin_svc = lin_svc_best.predict(X_test)
+
+# evaluating the best model now 
+#print(confusion_matrix(y_test, y_pred_lin_svc))
+#print(classification_report(y_test, y_pred_lin_svc))
+
+# we see a 93% test accuracy for this linear SVC model, which is good, and right in line with the other models 
+
+# we see that this model also does quite well, with a test accuracy of 95%. 
+# we will now use the cross validation for the kernel SVM model, and we will find the best parameters for this 
+from sklearn.svm import SVC 
+
+# establishing our parameter grid
+'''
+svc_param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
+                  'kernel': ['linear', 'poly', 'rbf', 'sigmoid']}
 
 svc_gs = GridSearchCV(SVC(), svc_param_grid, 
                       cv = 5)
 
-svc_grid = svc_gs.fit(X_train, y_train) 
+svc_grid = svc_gs.fit(X_train, y_train)
 
-print("Finding the best Linear SVM model...") 
+print("Finding the best Kernel SVM model...") 
 print(svc_grid.best_params_) 
 print(svc_grid.best_score_) 
+'''
+# we have a C value of 10 and a polynomial kernel with an impressive 98% train accuracy, so we will now fit the test data on this best model 
+svc_best = SVC(C = 10, kernel = 'poly').fit(X_train, y_train)
 
-# we can now see that the best linear SVC has a C value of 10 and a gamma value of 0.01.
-# we will make predictions with this best model now 
-svc_best = SVC(C = 10, gamma = 0.01).fit(X_train, y_train) 
 y_pred_svc = svc_best.predict(X_test)
 
-# evaluating the best model now 
-print(confusion_matrix(y_test, y_pred_svc))
-print(classification_report(y_test, y_pred_svc))
+# evaluating on the test data now 
+print(confusion_matrix(y_test, y_pred_svc)) 
+print(classification_report(y_test, y_pred_svc)) 
 
-# we see that this model also does quite well, with a test accuracy of 95%. 
+# we see that this model also does quite well, with a test accuracy f1 score of 95%.
+# we can include this information in the write up and then move on to problem 2. 
+
+############################################################################################################
+# Problem 2 
+# Write a function to perform 5-fold cross-validation for the tuning parameter associated with regularized logistic regression 
+# compare and contrast results for minimizing the CV error, and employing the one standard error rule 
+# associated with the following loss functions 
+
+# 1. Missclassification error 
+# 2. Binomial deviance loss 
+# 3. Hinge loss 
+
+# how do the selected tuning parameters perform in terms of test error? 
+
+############################################################################################################ 
+
+# how can we select a model within one standard error of the minimum cross-validation error?
+# we will introduce this for our regularized logistic regression model, and we will compare the two methods 
+# for the three loss functions. 
+
+# we will write a function for 5-fold cross-validation for the tuning parameter associated with regularized logistic regression, 
+# which in this case we can 
+
+# we can get the standard error by importing this from scipy 
+from scipy.stats import sem 
+
+# we can start to tune our regularized logistic regression here 
+'''
+def cross_val_tune(X, y, loss, param_grid):
+    # easier if we import this 
+    from sklearn.model_selection import KFold 
+
+    kf = KFold() # from the question of the problem itself, keeping defaults otherwise 
+
+    # init the empty list for the cross-validation error 
+    cv_errors = []
+    
+
+
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        # now doing grid search for the tuning hyperparameter in our case 
+        lr_gs = GridSearchCV(LogisticRegression(), param_grid, 
+                             cv = 5, scoring = loss)  
+
+        # now fitting the model 
+        lr_grid = lr_gs.fit(X_train, y_train) 
+
+        # keeping track of the errors 
+        cv_errors.append(1 - lr_grid.best_score_)
+
+    return cv_errors
+''' 
+# now we can try and call this function and see what we get, just using default loss to make sure it works
+from sklearn.model_selection import cross_val_score 
+
+# what are we actually trying to do?
+# we are trying to find the best C value for the logistic regression model, and we are using 5-fold cross validation to do so 
+# for the C value, we are either picking the one that minimizes the loss function for the three loss functions, or we are picking 
+# the largest one within one standard error of the minimum cross-validation error. 
+# we are applying this general process for three loss functions, and we are comparing the results. 
+
+# we can try and get the cross-validation error for the logistic regression model now, just using GridSearchCV to get the minimum lambda, 
+# whatever that hyperparameter is 
+
+param_grid_lr = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+
+lr_gs = GridSearchCV(LogisticRegression(), param_grid = param_grid_lr) 
+
+lr_grid = lr_gs.fit(X_train, y_train) # fitting the model 
+
+# now we can get the cross-validation error for the logistic regression model, 
+# and return our parameters for our best performing model in terms of cross validation error 
+# we can think about misclassification rate as 1-accuracy, and we can think about hinge loss as the hinge loss function 
+
+# we'll get our best parameters and then the lowest cross validation error in terms of misclassification rate next 
+print(lr_grid.best_params_) 
+print(1 - lr_grid.best_score_) 
+
+# we see that our best parameter is a C value of 0.1, and the lowest cross-validation error in terms of mis-classification rate is 0.05. 
+
+# now we can repeat this process in terms of hinge loss 
+'''
+
+lr_gs_hinge = GridSearchCV(LogisticRegression(), param_grid = param_grid_lr, 
+                           scoring = 'hinge_loss')
+
+lr_grid_hinge = lr_gs_hinge.fit(X_train, y_train) 
+
+# now getting best parameters and the lowest cross-validation error in terms of hinge loss 
+print(lr_grid_hinge.best_params_) 
+print(1 - lr_grid_hinge.best_score_) 
+'''
+
+
+# trying to implement hinge loss in another way 
+# we'll stick with our best model from before, and we'll try to get the hinge loss 
+from sklearn.metrics import hinge_loss 
+
+lr_best_hinge = LogisticRegression(C = 0.1).fit(X_train, y_train) 
+
+y_pred_hinge = lr_best_hinge.predict(X_test) 
+
+print(y_pred_hinge.shape) 
+
+# will have to re-shape for the hinge loss with labels 
+# including the labels 
+labels = np.unique(y_test)
+
+# we will have to change the shape of the predictions to include the labels, so we want (1009, 6) rather htan the current (1009,)  
+y_pred_hinge = np.array([y_pred_hinge == label for label in labels]).T 
+
+# evaluating the hinge loss 
+lr_hinge_l = hinge_loss(y_test , y_pred_hinge, labels = labels) 
+
+print(lr_hinge_l) 
+
+
+
 
 
 
